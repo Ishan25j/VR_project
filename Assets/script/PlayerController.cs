d92af24bb6ff5b9fa42871475fabb7619f2f7389
Ishan25j <ishan25j@gmail.com>
2023-06-04 10:15:25 +0530

    first commit

---
+++ Assets/script/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform[] spheres;
+    
+    public void updatePosition(int i)
+    {
+        transform.position = spheres[i].position;
+    }
+}
